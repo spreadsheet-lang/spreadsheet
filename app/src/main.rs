246cd9a161a484d3345f5bbfdaf07a9cc19020b4
Oli Scherer <github@oli-obk.de>
2024-10-16 21:47:20 +0200

    Cache `dx`

--- app/src/main.rs
+++ app/src/main.rs
@@ -7,8 +7,6 @@ use dioxus_logger::tracing::{info, Level};
 enum Route {
     #[route("/")]
     Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
 }
 
 fn main() {
@@ -23,25 +21,13 @@ fn App() -> Element {
         Router::<Route> {}
     }
 }
-#[component]
-fn Blog(id: i32) -> Element {
-    rsx! {
-        Link { to: Route::Home {}, "Go to counter" }
-        "Blog post {id}"
-    }
-}
+
 
 #[component]
 fn Home() -> Element {
     let mut count = use_signal(|| 0);
 
     rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog"
-        }
         div {
             h1 { "High-Five counter: {count}" }
             button { onclick: move |_| count += 1, "Up high!" }
