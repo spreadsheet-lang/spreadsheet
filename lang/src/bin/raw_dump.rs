50bda371b819949fb9b71cb3688d66b2b21f8323
Oli Scherer <github@oli-obk.de>
2024-10-18 17:20:48 +0200

    Test parse failures

--- lang/src/bin/raw_dump.rs
+++ lang/src/bin/raw_dump.rs
@@ -1,12 +1,28 @@
 use std::io;
 
-fn main() -> Result<(), io::Error> {
+#[derive(Debug)]
+enum Error {
+    Io(#[allow(dead_code)] io::Error),
+    Parse,
+}
+
+impl From<io::Error> for Error {
+    fn from(v: io::Error) -> Self {
+        Self::Io(v)
+    }
+}
+
+fn main() -> Result<(), Error> {
     let path = std::env::args().nth(1).unwrap();
     let input = std::fs::read_to_string(path)?;
     let output = lang::parse(&input);
-    assert!(output.errors.is_empty());
-    dbg(output.red_tree(), 0);
-    Ok(())
+    if output.errors.is_empty() {
+        dbg(output.red_tree(), 0);
+        Ok(())
+    } else {
+        eprintln!("{:#?}", output.errors);
+        Err(Error::Parse)
+    }
 }
 
 fn dbg(node: lang::SyntaxNode, indent: usize) {
