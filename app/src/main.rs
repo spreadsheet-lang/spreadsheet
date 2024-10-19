abdc9e1adb80668ea0ae730cf60f7efe2f54d851
Oli Scherer <github@oli-obk.de>
2024-10-19 15:09:38 +0200

    Add row/col rendering and movement

--- app/src/main.rs
+++ app/src/main.rs
@@ -22,16 +22,41 @@ fn App() -> Element {
     }
 }
 
-
 #[component]
 fn Home() -> Element {
-    let mut count = use_signal(|| 0);
+    let mut row = use_signal(|| 0_u128);
+    let mut col = use_signal(|| 0);
 
+    let col_name = name_from_index(*col.read());
+    let left = if *col.read() == 0 {
+        rsx! {button { disabled: true, "<" }}
+    } else {
+        rsx! {button { onclick: move |_| col -= 1, "<" }}
+    };
+    let up = if *row.read() == 0 {
+        rsx! {button { disabled: true, "^" }}
+    } else {
+        rsx! {button { onclick: move |_| row -= 1, "^" }}
+    };
     rsx! {
         div {
-            h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
+            h1 { "{col_name}{row}" }
+            {left}
+            button { onclick: move |_| col += 1, ">" }
+            {up}
+            button { onclick: move |_| row += 1, "v" }
+        }
+    }
+}
+
+fn name_from_index(mut col: u128) -> String {
+    let mut s = String::new();
+    loop {
+        let rem = (col % 26) as u8;
+        col /= 26;
+        s.insert(0, (b'A' + rem).into());
+        if col == 0 {
+            break s;
         }
     }
 }
