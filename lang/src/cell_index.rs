60680c1b2dcd0911b736bb00f63a51f66cd86899
Oli Scherer <github@oli-obk.de>
2024-10-27 07:10:49 +0100

    Add parser for col and row at the same time

--- lang/src/cell_index.rs
+++ lang/src/cell_index.rs
@@ -1,5 +1,6 @@
+use core::str;
 use std::fmt::Display;
-use std::num::NonZeroU128;
+use std::num::{NonZeroU128, ParseIntError};
 use std::ops::{Add, Sub};
 use std::str::FromStr;
 
@@ -82,25 +83,31 @@ impl Display for Col {
     }
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ParseColError;
 
 impl FromStr for Col {
     type Err = ParseColError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut col = 0_u128;
-        for c in s.bytes() {
-            match c {
-                b'A'..=b'Z' => {
-                    col *= u128::from(b'Z' - b'A' + 1);
-                    col += u128::from(c - b'A' + 1);
-                }
-                _ => return Err(ParseColError),
-            }
+        let mut bytes = s.as_bytes();
+        let res = parse_col(&mut bytes)?;
+        if bytes.is_empty() {
+            Ok(res)
+        } else {
+            Err(ParseColError)
         }
-        Ok(Self(col - 1))
     }
+}
+
+fn parse_col(bytes: &mut &[u8]) -> Result<Col, ParseColError> {
+    let mut col = 0_u128;
+    while let Some((c @ b'A'..=b'Z', rest)) = bytes.split_first() {
+        col *= u128::from(b'Z' - b'A' + 1);
+        col += u128::from(*c - b'A' + 1);
+        *bytes = rest;
+    }
+    Ok(Col(col.checked_sub(1).ok_or(ParseColError)?))
 }
 
 #[test]
@@ -111,4 +118,50 @@ fn col_roundtrip() {
     assert_eq!(Col::FIRST.to_string().parse(), Ok(Col::FIRST));
     assert_eq!("AA".parse(), Ok(Col(26)));
     assert_eq!("DD".parse(), Ok(Col(26 * 4 + 3)));
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ParseCellIndexError {
+    Col(ParseColError),
+    Row(ParseIntError),
+}
+
+impl From<ParseIntError> for ParseCellIndexError {
+    fn from(v: ParseIntError) -> Self {
+        Self::Row(v)
+    }
+}
+
+impl From<ParseColError> for ParseCellIndexError {
+    fn from(v: ParseColError) -> Self {
+        Self::Col(v)
+    }
+}
+
+pub fn parse(s: &str) -> Result<(Col, Row), ParseCellIndexError> {
+    let mut bytes = s.as_bytes();
+    let col = parse_col(&mut bytes)?;
+    let s = str::from_utf8(bytes)
+        .expect("parse_col only consumes A-Z, so we should be left with valid utf8");
+
+    let row = Row::from_str(s)?;
+    Ok((col, row))
+}
+
+#[test]
+fn cell_parse() {
+    assert_eq!(parse("A1"), Ok((Col(0), Row(NonZeroU128::new(1).unwrap()))));
+    assert_eq!(parse("B5"), Ok((Col(1), Row(NonZeroU128::new(5).unwrap()))));
+    assert_eq!(
+        parse("Z12"),
+        Ok((Col(25), Row(NonZeroU128::new(12).unwrap())))
+    );
+    assert_eq!(
+        parse("AA3"),
+        Ok((Col(26), Row(NonZeroU128::new(3).unwrap())))
+    );
+    assert_eq!(
+        parse("DD0000009"),
+        Ok((Col(26 * 4 + 3), Row(NonZeroU128::new(9).unwrap())))
+    );
 }
