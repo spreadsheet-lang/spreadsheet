246e51c69880bb17d7aae67adba5a1b22e092dc8
Oli Scherer <github@oli-obk.de>
2024-10-27 06:36:12 +0100

    Implement parsing col/row from strings

--- lang/src/cell_index.rs
+++ lang/src/cell_index.rs
@@ -1,8 +1,9 @@
 use std::fmt::Display;
 use std::num::NonZeroU128;
 use std::ops::{Add, Sub};
+use std::str::FromStr;
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct Row(NonZeroU128);
 
 impl Display for Row {
@@ -10,6 +11,15 @@ impl Display for Row {
         self.0.fmt(f)
     }
 }
+
+impl FromStr for Row {
+    type Err = <NonZeroU128 as FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        NonZeroU128::from_str(s).map(Self)
+    }
+}
+
 impl Add<u128> for Row {
     type Output = Option<Row>;
 
@@ -34,7 +44,7 @@ impl Row {
     pub const FIRST: Self = Self(NonZeroU128::MIN);
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct Col(u128);
 
 impl Add<u128> for Col {
@@ -70,4 +80,35 @@ impl Display for Col {
             }
         }
     }
+}
+
+#[derive(PartialEq, Debug)]
+pub struct ParseColError;
+
+impl FromStr for Col {
+    type Err = ParseColError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut col = 0_u128;
+        for c in s.bytes() {
+            match c {
+                b'A'..=b'Z' => {
+                    col *= u128::from(b'Z' - b'A' + 1);
+                    col += u128::from(c - b'A' + 1);
+                }
+                _ => return Err(ParseColError),
+            }
+        }
+        Ok(Self(col - 1))
+    }
+}
+
+#[test]
+fn col_roundtrip() {
+    assert_eq!("A".parse(), Ok(Col(0)));
+    assert_eq!("B".parse(), Ok(Col(1)));
+    assert_eq!("Z".parse(), Ok(Col(25)));
+    assert_eq!(Col::FIRST.to_string().parse(), Ok(Col::FIRST));
+    assert_eq!("AA".parse(), Ok(Col(26)));
+    assert_eq!("DD".parse(), Ok(Col(26 * 4 + 3)));
 }
